4d53add2ab620a7d9907435a86a4040280c2a7fd
Oscar Giner <osgial@alumnatflorida.es>
2025-09-09 10:14:14 +0200

    ap1-1.php terminada

--- bloque_1/ap1-1/ap1-1.php
+++ bloque_1/ap1-1/ap1-1.php
@@ -1,2 +1,23 @@
 <?php
+$array = [];
+$num=1;
+
+foreach ($_GET as $clave => $valor) {
+        $array[$clave] = $valor;
+}
+/*
+$valor1=$_GET["clave1"];
+$valor2=$_GET["clave2"];
+
+$array=[
+    "clave1"=>$valor1,
+    "clave2"=>$valor2,
+];
+*/
+
+foreach ($array as $clave => $valor) {
+    echo "Se ha recibido $valor para la $clave.<br>";
+    $num++;
+}
+
 echo "Hello World!";
